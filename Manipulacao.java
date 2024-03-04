adfef64894412e95180dbb29fff3124e6bec4721
Gabriel Marcolin de Carvalho <146651956+Marcoliiin@users.noreply.github.com>
2024-03-04 12:27:10 -0300

    Create Manipulacao.java

---
+++ Manipulacao.java
@@ -0,0 +1,10 @@
+public class Manipulacao {
+    public static void main(String[] args) {
+
+
+        Produto.cadastrar_produto();
+        Fornecedor.criar_fornecedor();
+        Clientes.criar_cliente();
+
+    }
+}
