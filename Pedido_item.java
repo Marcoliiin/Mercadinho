1348ac6a01ba30cb3aaddfe94dab763cca915ff9
Gabriel Marcolin de Carvalho <146651956+Marcoliiin@users.noreply.github.com>
2024-03-04 17:24:08 -0300

    Create Pedido_item.java
    classe que eu criei para vincular o pedido com o produto, produto, não faz mt sentido pois eu literalmente tenho uma classe universal para isso.
    
    Eu tenho uma classe de venda_item, que obviamente é necessária, porém, não preciso de uma classe para adiconar coisas nesta tabela já que eu tenho a classe Vinculos.

---
+++ Pedido_item.java
@@ -0,0 +1,42 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Pedido_item {
+    public static void main(String[] args) {
+    }
+
+    public static int consultar_id_produto() {
+        int retorno_consulta_produto = 0;
+
+        try (Scanner sc = new Scanner(System.in);
+             Connection connection = Conexao.getConnection()) {
+
+            System.out.println("Qual o ID do item que foi vendido?");
+            int id_item = sc.nextInt();
+
+            String sql = "SELECT ID from produto where id = ?";
+            try (PreparedStatement consultando_id = connection.prepareStatement(sql)) {
+                consultando_id.setInt(1, id_item);
+
+                try (ResultSet query = consultando_id.executeQuery()) {
+                    if (query.next()) {
+                        retorno_consulta_produto = query.getInt(1);
+                    }
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException();
+        }
+        return retorno_consulta_produto;
+    }
+
+    public static int consultar_vendedor_id_fornecedor(){
+
+      return 0;
+    }
+}
