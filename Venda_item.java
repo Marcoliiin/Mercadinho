5cfcee771a62375ca9c190e6b890608a7d84e489
Gabriel Marcolin de Carvalho <146651956+Marcoliiin@users.noreply.github.com>
2024-03-04 21:37:19 -0300

    Update Venda_item.java

--- Venda_item.java
+++ Venda_item.java
@@ -4,7 +4,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
-public class _item {
+public class Venda_item {
     public static void main(String[] args) {
     }
 
