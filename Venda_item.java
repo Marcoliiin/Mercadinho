2c24234ac6fa6501b18faa83bd281432a6ab8720
Gabriel Marcolin de Carvalho <146651956+Marcoliiin@users.noreply.github.com>
2024-03-05 14:11:33 -0300

    Update Venda_item.java

--- Venda_item.java
+++ Venda_item.java
@@ -1,28 +1,18 @@
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Venda_item {
     public static void main(String[] args) {
-    }
-    
-    try (Scanner sc = new Scanner(System.in);
-    Connection connection = Conexao.getConnection()){
+        try (Scanner sc = new Scanner(System.in);
+             Connection connection = Conexao.getConnection()) {
 
-        System.out.println("Qual o ID do produto desta venda?");
-        int id_produto = sc.nextInt();
 
-        String sql = "SELECT ID from produto where id = ?";
-        try(prepareStatement consultando_id = connection.PreparedStatement(sql)){
-            inserindo.setInt(1,id_produto);
+       int id_venda = Consultar_ids.consultar_id_venda();
 
-            try (ResultSet query = consultando_id.executeQuery()) {
-                    if (query.next()) {
-                        retorno_consulta_produto = query.getInt(1);
-                    }
-                }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
         }
     }
 }
