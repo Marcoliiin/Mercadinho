5c0f1158b14a4a9a0f010666723daa12bb7771e0
Gabriel Marcolin de Carvalho <146651956+Marcoliiin@users.noreply.github.com>
2024-03-06 16:44:03 -0300

    Update Consultar_ids.java
    aparentemente, colocar o scanner dentro do try estava dando o erro de NoNextLineFound, finalmenteeeeeeeeeeeeee

--- Consultar_ids.java
+++ Consultar_ids.java
@@ -1,76 +1,136 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.ResultSet;
 import java.util.Scanner;
 
-public class Venda {
-    public static void main(String[] args) {
-
-        long id_produto = 0;
-        long id_cliente = 0;
-        long id_vendedor = 0;
-        int valor_total = 0;
-        int quantidade_vendida = 0;
+public class Consultar_ids {
+    public static long consultar_id_fornecedor() {
+        long retorno_fornecedor = 0;
         Scanner sc = new Scanner(System.in);
 
-        Venda venda = new Venda(id_produto, id_cliente, id_vendedor, quantidade_vendida, valor_total, sc);
-    }
-
-    public long id_produto;
-    public long id_cliente;
-    public long id_vendedor;
-    public int valor_total;
-    public int quantidade_vendida;
-
-    public Venda(long id_produto, long id_cliente, long id_vendedor, int valor_total, int quantidade_vendida, Scanner sc) {
-        System.out.println("Qual a quantidade vendida deste produto?");
-        this.quantidade_vendida = sc.nextInt();
-
-        this.id_produto = Consultar_ids.consultar_id_produto();
-        this.id_cliente = Consultar_ids.consultar_id_cliente();
-        this.id_vendedor = Consultar_ids.consultar_id_vendedor();
-        this.valor_total = (this.quantidade_vendida * consultar_preco(this.id_produto));
-
-        cadastrar_pedido(this.id_vendedor, this.id_cliente, this.valor_total);
-    }
-
-    public static void cadastrar_pedido(long id_vendedor, long id_cliente, long valor_total) {
         try (Connection connection = Conexao.getConnection()) {
 
-            String sql = "INSERT INTO venda (id_cliente,id_vendedor,valor_total) VALUES (?,?,?)";
-            try (PreparedStatement inserindo = connection.prepareStatement(sql)) {
-                inserindo.setLong(1, id_cliente);
-                inserindo.setLong(2, id_vendedor);
-                inserindo.setLong(3, valor_total);
+            System.out.println("Qual é o ID do fornecedor deste produto?");
+            long id_fornecedor = sc.nextLong();
 
-                inserindo.executeUpdate();
+            String sql = "SELECT id FROM fornecedor WHERE id = ?";
+            try (PreparedStatement consultando_id = connection.prepareStatement(sql)) {
+                consultando_id.setLong(1, id_fornecedor);
 
+                try (ResultSet query = consultando_id.executeQuery()) {
+                    if (query.next()) {
+                        retorno_fornecedor = query.getLong(1);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return retorno_fornecedor;
+    }
+
+    public static long consultar_id_cliente() {
+        long retorno_cliente = 0;
+        Scanner sc = new Scanner(System.in);
+        try (Connection connection = Conexao.getConnection()) {
+
+            System.out.println("Qual é o ID do cliente da venda?");
+            long id_cliente = sc.nextLong();
+
+            String sql = "SELECT id FROM clientes WHERE id = ?";
+            try (PreparedStatement consultando_id = connection.prepareStatement(sql)) {
+                consultando_id.setLong(1, id_cliente);
+
+                try (ResultSet query = consultando_id.executeQuery()) {
+                    if (query.next()) {
+                        retorno_cliente = query.getLong(1);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return retorno_cliente;
+    }
+
+    public static long consultar_id_vendedor() {
+        long retorno_vendedor = 0;
+        Scanner sc = new Scanner(System.in);
+
+        try (Connection connection = Conexao.getConnection()) {
+
+            System.out.println("Qual é o ID do vendedor da venda?");
+            long id_vendedor = sc.nextLong();
+
+            String sql = "SELECT id FROM vendedor WHERE id = ?";
+            try (PreparedStatement consultando_id = connection.prepareStatement(sql)) {
+                consultando_id.setLong(1, id_vendedor);
+
+                try (ResultSet query = consultando_id.executeQuery()) {
+                    if (query.next()) {
+                        retorno_vendedor = query.getLong(1);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return retorno_vendedor;
+    }
+
+    public static long consultar_id_produto() {
+        long retorno_produto = 0;
+        Scanner sc = new Scanner(System.in);
+
+        try (Connection connection = Conexao.getConnection()) {
+
+            System.out.println("Qual o ID do item que foi vendido?");
+            long id_item = sc.nextLong();
+
+            String sql = "SELECT ID from produto where id = ?";
+            try (PreparedStatement consultando_id = connection.prepareStatement(sql)) {
+                consultando_id.setLong(1, id_item);
+
+                try (ResultSet query = consultando_id.executeQuery()) {
+                    if (query.next()) {
+                        retorno_produto = query.getLong(1);
+                    }
+                }
             } catch (SQLException e) {
                 e.printStackTrace();
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException();
+        }
+        return retorno_produto;
+    }
+
+    public static long consultar_id_venda() {
+        long retorno_venda = 0;
+        Scanner sc = new Scanner(System.in);
+
+        try (Connection connection = Conexao.getConnection()) {
+
+            System.out.println("Qual o ID da venda?");
+            long id_venda = sc.nextLong();
+
+            String sql = "SELECT ID from venda where ID = ?";
+            try (PreparedStatement consultando_id = connection.prepareStatement(sql)) {
+                consultando_id.setLong(1, id_venda);
+
+                try (ResultSet query = consultando_id.executeQuery()) {
+                    if (query.next()) {
+                        retorno_venda = query.getLong(1);
+                    }
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
             }
         } catch (SQLException e) {
             throw new RuntimeException();
         }
-    }
-
-    public static int consultar_preco(long id_produto) {
-        int preco_produto = 0;
-
-        try (Connection connection = Conexao.getConnection()) {
-
-            String sql = "SELECT preco FROM produto where id = ?";
-            try (PreparedStatement consultando_preco = connection.prepareStatement(sql)) {
-                consultando_preco.setLong(1, id_produto);
-
-                ResultSet query = consultando_preco.executeQuery();
-                if (query.next()) {
-                    preco_produto = query.getInt(1);
-                }
-            }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-        return preco_produto;
+        return retorno_venda;
     }
 }
