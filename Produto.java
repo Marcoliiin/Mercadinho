021006585fa8dc19472d1008ac37e744f79ee558
Gabriel Marcolin de Carvalho <146651956+Marcoliiin@users.noreply.github.com>
2024-03-07 09:42:38 -0300

    Update Produto.java

--- Produto.java
+++ Produto.java
@@ -4,33 +4,33 @@ import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Produto {
- public static void main(String[] args){
-    String descricao = "";
-    double preco = 0;
-    int estoque = 0;
-    Scanner sc = new Scanner(System.in);
- }
+    public static void main(String[] args){
+        String descricao = "";
+        double preco = 0;
+        int estoque = 0;
+        Scanner sc = new Scanner(System.in);
+    }
 
- public String descricao;
- public double preco = 0;
- public int estoque = 0;
+    public String descricao;
+    public double preco = 0;
+    public int estoque = 0;
 
     public Produto(String descricao,double preco,int estoque,Scanner sc) {
-            System.out.println("Qual é a descrição do produto?");
-            this.descricao    = sc.nextLine();
-    
-            System.out.println("Qual o preço deste produto?");
-            this.preco = sc.nextDouble();
+        System.out.println("Qual é a descrição do produto?");
+        this.descricao    = sc.nextLine();
 
-            System.out.println("Qual é o estoque deste produto?");
-            this.estoque = sc.nextInt();
+        System.out.println("Qual o preço deste produto?");
+        this.preco = sc.nextDouble();
 
-            produto_vinculo(this.descricao, this.preco, this.estoque);
+        System.out.println("Qual é o estoque deste produto?");
+        this.estoque = sc.nextInt();
+
+        cadastrar_produto(this.descricao, this.preco, this.estoque);
     }
 
     public static void cadastrar_produto(String descricao, double preco, int estoque) {
 
-        int id_fornecedor = Consultar_ids.consultar_id_fornecedor();
+        long id_fornecedor = Consultar_ids.consultar_id_fornecedor();
 
         try (Connection connection = Conexao.getConnection()) {
             String sintaxe = "INSERT INTO produto (descricao,preco,estoque,id_fornecedor) VALUES (?,?,?,?)";
@@ -38,7 +38,7 @@ public class Produto {
                 inserindo.setString(1, descricao);
                 inserindo.setDouble(2, preco);
                 inserindo.setInt(3, estoque);
-                inserindo.setInt(4, id_fornecedor);
+                inserindo.setLong(4, id_fornecedor);
 
                 inserindo.executeUpdate();
 
