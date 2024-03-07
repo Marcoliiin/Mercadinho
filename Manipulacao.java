0beabd403fef6aee4578a87c52c12fff1dad2d44
Gabriel Marcolin de Carvalho <146651956+Marcoliiin@users.noreply.github.com>
2024-03-07 09:42:23 -0300

    Update Manipulacao.java

--- Manipulacao.java
+++ Manipulacao.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Manipulacao {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
         long id_produto = 0;
         long id_cliente = 0;
@@ -11,6 +10,8 @@ public class Manipulacao {
         String descricao = "";
         double preco = 0;
         int estoque = 0;
+        int quantidade_vendida = 0;
+        int valor_total = 0;
         Scanner sc = new Scanner(System.in);
 
         System.out.println("O que você deseja fazer?\n[1]Cadastrar algo.\n[2]Consultar algo\n[3]Excluir algo\n[4]Sair");
@@ -22,19 +23,19 @@ public class Manipulacao {
                 escolha = sc.nextInt();
                 switch (escolha) {
                     case 1:
-                       Produto produto = new Produto(String descricao, double preco,int estoque,Scanner sc);
+                        Produto produto = new Produto(descricao, preco, estoque, sc);
                         break;
                     case 2:
-                       Venda venda = new Venda(long id_produto,long id_cliente,long id_produto,int quantidade_vendida, Scanner sc );
+                        Venda venda = new Venda(id_produto, id_cliente, id_vendedor, quantidade_vendida, valor_total, sc);
                         break;
                     case 3:
-                        Clientes.criar_cliente();
+                        Clientes clientes = new Clientes();
                         break;
                     case 4:
-                        Fornecedor.criar_fornecedor();
+                        Fornecedor Fornecedor = new Fornecedor();
                         break;
                     case 5:
-                        Vendedor.criar_vendedor();
+                        Vendedor vendedor = new Vendedor();
                         break;
                     default:
                         System.out.println("Esta não é uma opção válida.");
