986e2b841534c2926be634b7897a1bc300508b90
Gabriel Marcolin de Carvalho <146651956+Marcoliiin@users.noreply.github.com>
2024-03-08 17:29:16 -0300

    Update and rename VendaItem.java to SaleItem.java

---
+++ src/main/java/SaleItem.java
@@ -0,0 +1,10 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class SaleIten {
+    public static void main(String[] args) {
+
+    }
+}
