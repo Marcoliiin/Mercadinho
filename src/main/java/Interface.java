541d3a8237c6a629318ed9f5bb3569180e72d73b
Gabriel Marcolin de Carvalho <146651956+Marcoliiin@users.noreply.github.com>
2024-03-08 13:45:21 -0300

    Update and rename Manipulacao.java to Interface.java

--- src/main/java/Interface.java
+++ src/main/java/Interface.java
@@ -1,41 +1,41 @@
 import java.util.Scanner;
 
-public class Manipulacao {
+public class Interface {
     public static void main(String[] args) {
 
-        long id_produto = 0;
-        long id_cliente= 0;
-        long id_vendedor= 0;
-        long id_fornecedor= 0;
-        String descricao= "";
-        double preco= 0;
-        int estoque= 0;
-        int quantidade_vendida= 0;
-        int valor_total= 0;
+        long productId = 0;
+        long clientId= 0;
+        long sellerId= 0;
+        long supplierId= 0;
+        String description= "";
+        double price = 0;
+        int stock = 0;
+        int quantitySold= 0;
+        int totalValue= 0;
         Scanner sc = new Scanner(System.in);
 
         System.out.println("O que você deseja fazer?\n[1]Cadastrar algo.\n[2]Consultar algo\n[3]Excluir algo\n[4]Sair");
-        int escolha = sc.nextInt();
-        switch (escolha) {
+        int choise = sc.nextInt();
+        switch (choise) {
             case 1:
                 System.out.println("O que vocẽ deseja cadastrar?\n[1]Produtos\n[2]Pedido");
                 System.out.println("[3]Cliente\n[4]Fornecedor[5]Vendedor");
-                escolha = sc.nextInt();
-                switch (escolha) {
+                choise = sc.nextInt();
+                switch (choise) {
                     case 1:
-                        Produto produto = new Produto(descricao, preco, estoque, sc);
+                        Product product = new Product(description, price, stock, sc);
                         break;
                     case 2:
-                        Venda venda = new Venda(id_produto, id_cliente, id_vendedor, quantidade_vendida, valor_total, sc);
+                        Sale sale = new Sale(productId, clientId, sellerId, quantitySold, totalValue, sc);
                         break;
                     case 3:
-                        Clientes clientes = new Clientes();
+                        Clients clients = new Clients();
                         break;
                     case 4:
-                        Fornecedor Fornecedor = new Fornecedor();
+                        Supplier supplier = new Supplier();
                         break;
                     case 5:
-                        Vendedor vendedor = new Vendedor();
+                        Seller seller = new Seller();
                         break;
                     default:
                         System.out.println("Esta não é uma opção válida.");
