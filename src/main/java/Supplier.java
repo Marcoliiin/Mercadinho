3d0d9fc2720a5f5d7f25c4f7621d6a44f73f8873
Gabriel Marcolin de Carvalho <146651956+Marcoliiin@users.noreply.github.com>
2024-03-08 13:45:05 -0300

    Update and rename Fornecedor.java to Supplier.java
    Convenções

---
+++ src/main/java/Supplier.java
@@ -0,0 +1,40 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Supplier {
+
+    String description;
+    String contact;
+    String adress;
+    Scanner sc = new Scanner(System.in);
+
+    public Supplier() {
+        System.out.println("Digite o nome do seu fornecedor: ");
+        this.description = sc.nextLine();
+        System.out.println("Digite o contato do seu fornecedor: ");
+        this.contact = sc.nextLine();
+        System.out.println("Digite o endereço do seu fornecedor: ");
+        this.adress = sc.nextLine();
+
+        createSupplier(description,contact,adress);
+    }
+
+    public static void createSupplier(String description, String contact, String adress) {
+        try (Connection connection = Connecting.getConnection()) {
+
+            String sql = "INSERT INTO fornecedor (descricao,contato,endereco) VALUES (?,?,?)";
+            try (PreparedStatement enteringSupplier = connection.prepareStatement(sql)) {
+                enteringSupplier.setString(1, description);
+                enteringSupplier.setString(2, contact);
+                enteringSupplier.setString(3, adress);
+                enteringSupplier.executeUpdate();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException();
+        }
+    }
+}
