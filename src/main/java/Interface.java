8047997046ec9aeb24189a722465d12c835cd6a0
Gabriel Marcolin de Carvalho <146651956+Marcoliiin@users.noreply.github.com>
2024-03-09 12:06:28 -0300

    Update Interface.java

--- src/main/java/Interface.java
+++ src/main/java/Interface.java
@@ -14,7 +14,7 @@ public class Interface {
         int totalValue = 0;
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("O que você deseja fazer?\n[1]Cadastrar algo.\n[2]Consultar algo\n[3]Excluir algo.\n[4]Sair.");
+        System.out.println("O que você deseja fazer?\n[1]Cadastrar algo.\n[2]Consultar algo.\n[3]Excluir algo.\n[4]Sair.");
         int choise = sc.nextInt();
         sc.nextLine();
 
@@ -41,6 +41,29 @@ public class Interface {
                         break;
                     case 5:
                         Seller seller = new Seller();
+                        break;
+                    default:
+                        System.out.println("Esta não é uma opção válida.");
+                        break;
+                }
+            case 2:
+                System.out.println("O que você deseja consultar?\n[1]Produto.\n[2]Pedido.");
+                System.out.println("[3]Cliente.\n[4]Fornecedor.\n[5]Vendedor.");
+                choise = sc.nextInt();
+                sc.nextLine();
+
+                switch (choise) {
+                    case 1:
+                        Search.searchingProduct();
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        Search.searchingClient();
+                        break;
+                    case 4:
+                        break;
+                    case 5:
                         break;
                     default:
                         System.out.println("Esta não é uma opção válida.");
