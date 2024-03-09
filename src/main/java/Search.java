dd831fe94df8e09c7eb044637f101ce3e4246cd1
Gabriel Marcolin de Carvalho <146651956+Marcoliiin@users.noreply.github.com>
2024-03-09 12:06:10 -0300

    Update Search.java

--- src/main/java/Search.java
+++ src/main/java/Search.java
@@ -5,25 +5,57 @@ import java.sql.ResultSet;
 import java.util.Scanner;
 
 public class Search {
-    public static void searchingClients() {
-        try (Scanner sc = new Scanner(System.in)) {
+    public static void searchingClient() {
+        Scanner sc = new Scanner(System.in);
+        try {
             Connection connection = Connecting.getConnection();
 
             System.out.println("Qual é o seu nome?");
             String name = sc.nextLine();
 
-            String sql = "SELECT * FROM clientes where nome = ?";
-            PreparedStatement searchingClients = connection.prepareStatement(sql);
-            searchingClients.setString(1, name);
-            ResultSet resultado = searchingClients.executeQuery();
+            String sql = "SELECT * FROM clientes where UPPER(nome) = UPPER(?)";
+            PreparedStatement searchingClient = connection.prepareStatement(sql);
+            searchingClient.setString(1, name);
+            ResultSet resultSearch = searchingClient.executeQuery();
 
-            if (resultado.next()) {
-                System.out.println("ID do cliente: " + resultado.getInt(1));
-                System.out.println("Nome do cliente: " + resultado.getString(2));
-                System.out.println("Sexo do cliente: " + resultado.getString(3));
-                System.out.println("Endereço do cliente: " + resultado.getString(4));
-                System.out.println("Data do cadastro: " + resultado.getString(5));
+            if (resultSearch.next()) {
+                System.out.println("ID do cliente: " + resultSearch.getInt(1));
+                System.out.println("Nome do cliente: " + resultSearch.getString(2));
+                System.out.println("Sexo do cliente: " + resultSearch.getString(3));
+                System.out.println("Endereço do cliente: " + resultSearch.getString(4));
+                System.out.println("Data do cadastro: " + resultSearch.getString(5));
+            } else {
+                System.out.println("Não foi possível encontrar nenhum cliente com este nome.");
             }
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    public static void searchingProduct() {
+        Scanner sc = new Scanner(System.in);
+        try {
+            Connection connection = Connecting.getConnection();
+
+            System.out.println("Qual é a descrição do seu produto?");
+            String descricao = sc.nextLine();
+
+            String sql = "SELECT * FROM produto where UPPER(descricao) = UPPER(?)";
+            PreparedStatement searchingProduct = connection.prepareStatement(sql);
+            searchingProduct.setString(1, descricao);
+            ResultSet resultSearch = searchingProduct.executeQuery();
+
+            if (resultSearch.next()) {
+                System.out.println("ID do produto: " + resultSearch.getInt(1));
+                System.out.println("Descrição do produto: " + resultSearch.getString(2));
+                System.out.println("Preco do produto: " + resultSearch.getString(3));
+                System.out.println("Estoque do produto: " + resultSearch.getString(4));
+                System.out.println("ID do fornecedor do produto: " + resultSearch.getString(5));
+                System.out.println("Data do cadastro: " + resultSearch.getString(6));
+            } else {
+                System.out.println("Não foi possível encontrar nenhum produto com esta descrição.");
+            }
+
         } catch (SQLException exception) {
             exception.printStackTrace();
         }
