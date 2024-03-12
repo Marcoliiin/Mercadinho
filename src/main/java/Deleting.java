00a110b2b94eebc07143086e64c9a9284b1b3bff
Gabriel Marcolin de Carvalho <146651956+Marcoliiin@users.noreply.github.com>
2024-03-12 08:48:00 -0300

    Update Deleting.java

--- src/main/java/Deleting.java
+++ src/main/java/Deleting.java
@@ -1,50 +1,49 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Deleting {
 
- private int entityId;
- private String table;
- Scanner sc = new Scanner(System.in);
+    private int entityId;
+    private String table;
+    Scanner sc = new Scanner(System.in);
 
- public static void deletingEntity(){
-    int entityId = 0;
-    String table = "";
-   System.out.println("CUIDADO!!!\n A exclusão de entidades NÃO PODE ser revertida!!!");
-  System.out.println("Qual é a tabela do produto que você deseja excluir?");
-  this.table = sc.nextString(); 
+    public Deleting(){
 
-  System.out.println("Qual é o ID da entidade que você quer deletar?");
-  this.entityId = sc.nextInt();
+        System.out.println("CUIDADO!!!\nA exclusão de entidades NÃO PODE ser revertida!!!");
+        System.out.println("Qual é a tabela do produto que você deseja excluir?");
+        this.table = sc.nextLine();
 
-  try{ 
-    Connection connection = Connecting.getConnecting();
-    
-    String sql = "DELETE * from " + this.table + " WHERE ID = ?";
-    try(PreparedStatement deletingEntity = connection.preparedStatement(sql)) {
-        deletingEntity.setInt(1,this.id);
+        System.out.println("Qual é o ID da entidade que você quer deletar?");
+        this.entityId = sc.nextInt();
 
- System.out.println("ATENÇÃO!!!\n Você realmente quer realizar a exclusão desta entidade?\n[1]Sim\n[2]Não");
- String choise = sc.nextLine();
+        deletingEntity();
+    }
 
- switch(choise){
-    case 1: 
-    deletingEntity.executeUpdate(sql);
-    break;
-    case 2:
-        return;
- }
+    public  void deletingEntity() {
+        try {
+            Connection connection = Connecting.getConnection();
 
+            String sql = "DELETE from " + this.table + " WHERE ID = ?";
+            try (PreparedStatement deletingEntity = connection.prepareStatement(sql)) {
+                deletingEntity.setInt(1, this.entityId);
 
-    }catch (SQLException exception){
-    exception.printStackTrace();
+                System.out.println("ATENÇÃO!!!\nVocê realmente quer realizar a exclusão desta entidade?\n[1]Sim\n[2]Não");
+                int choise = sc.nextInt();
 
-  } catch (SQLException exception){
-    exception.printStackTrace();
-  }
- }
-}
+                switch (choise) {
+                    case 1:
+                        deletingEntity.executeUpdate();
+                        break;
+                    case 2:
+                        return;
+                }
+            } catch (SQLException exception) {
+                exception.printStackTrace();
+            }
+        } catch (SQLException exception) {
+            exception.getMessage();
+        }
+    }
 }
