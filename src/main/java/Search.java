305aa7559af97bbf7480daf13c91a3784b7cebdb
Gabriel Marcolin de Carvalho <146651956+Marcoliiin@users.noreply.github.com>
2024-03-14 14:49:00 -0300

    Update Search.java

--- src/main/java/Search.java
+++ src/main/java/Search.java
@@ -12,23 +12,27 @@ public class Search {
 
             System.out.println("Qual é o seu nome?");
             String name = sc.nextLine();
+            try {
+                String sql = "SELECT * FROM clientes where UPPER(nome) = UPPER(?)";
+                PreparedStatement searchingClient = connection.prepareStatement(sql);
+                searchingClient.setString(1, name);
+                ResultSet resultSearch = searchingClient.executeQuery();
 
-            String sql = "SELECT * FROM clientes where UPPER(nome) = UPPER(?)";
-            PreparedStatement searchingClient = connection.prepareStatement(sql);
-            searchingClient.setString(1, name);
-            ResultSet resultSearch = searchingClient.executeQuery();
-
-            if (resultSearch.next()) {
-                System.out.println("ID do cliente: " + resultSearch.getInt(1));
-                System.out.println("Nome do cliente: " + resultSearch.getString(2));
-                System.out.println("Sexo do cliente: " + resultSearch.getString(3));
-                System.out.println("Endereço do cliente: " + resultSearch.getString(4));
-                System.out.println("Data do cadastro: " + resultSearch.getString(5));
-            } else {
-                System.out.println("Não foi possível encontrar nenhum cliente com este nome.");
+                if (resultSearch.next()) {
+                    System.out.println("ID do cliente: " + resultSearch.getInt(1));
+                    System.out.println("Nome do cliente: " + resultSearch.getString(2));
+                    System.out.println("Sexo do cliente: " + resultSearch.getString(3));
+                    System.out.println("Endereço do cliente: " + resultSearch.getString(4));
+                    System.out.println("Data do cadastro: " + resultSearch.getString(5));
+                } else {
+                    System.out.println("Não foi possível encontrar nenhum cliente com este nome.");
+                }
+            } catch (SQLException exception) {
+                System.err.println("Erro ao pesquisar o cliente: " + exception.getMessage());
+                exception.printStackTrace();
             }
         } catch (SQLException exception) {
-            exception.getMessage();
+            System.err.println("Erro ao se conectar ao banco: " + exception.getMessage());
             exception.printStackTrace();
         }
     }
@@ -40,25 +44,28 @@ public class Search {
 
             System.out.println("Qual é a descrição do seu produto?");
             String descricao = sc.nextLine();
+            try {
+                String sql = "SELECT * FROM produto where UPPER(descricao) = UPPER(?)";
+                PreparedStatement searchingProduct = connection.prepareStatement(sql);
+                searchingProduct.setString(1, descricao);
+                ResultSet resultSearch = searchingProduct.executeQuery();
 
-            String sql = "SELECT * FROM produto where UPPER(descricao) = UPPER(?)";
-            PreparedStatement searchingProduct = connection.prepareStatement(sql);
-            searchingProduct.setString(1, descricao);
-            ResultSet resultSearch = searchingProduct.executeQuery();
-
-            if (resultSearch.next()) {
-                System.out.println("ID do produto: " + resultSearch.getInt(1));
-                System.out.println("Descrição do produto: " + resultSearch.getString(2));
-                System.out.println("Preco do produto: " + resultSearch.getString(3));
-                System.out.println("Estoque do produto: " + resultSearch.getString(4));
-                System.out.println("ID do fornecedor do produto: " + resultSearch.getString(5));
-                System.out.println("Data do cadastro: " + resultSearch.getString(6));
-            } else {
-                System.out.println("Não foi possível encontrar nenhum produto com esta descrição.");
+                if (resultSearch.next()) {
+                    System.out.println("ID do produto: " + resultSearch.getInt(1));
+                    System.out.println("Descrição do produto: " + resultSearch.getString(2));
+                    System.out.println("Preco do produto: " + resultSearch.getString(3));
+                    System.out.println("Estoque do produto: " + resultSearch.getString(4));
+                    System.out.println("ID do fornecedor do produto: " + resultSearch.getString(5));
+                    System.out.println("Data do cadastro: " + resultSearch.getString(6));
+                } else {
+                    System.out.println("Não foi possível encontrar nenhum produto com esta descrição.");
+                }
+            } catch (SQLException exception) {
+                System.err.println("Erro ao pesquisar o produto: " + exception.getMessage());
+                exception.printStackTrace();
             }
-
         } catch (SQLException exception) {
-            exception.getMessage();
+            System.err.println("Erro ao se conectar ao banco: " + exception.getMessage());
             exception.printStackTrace();
         }
     }
@@ -71,23 +78,27 @@ public class Search {
             System.out.println("Qual é o ID do seu pedido?");
             long saleId = sc.nextLong();
             sc.nextLine();
+            try {
+                String sql = "SELECT * FROM venda WHERE id = ?";
+                PreparedStatement searchingSale = connection.prepareStatement(sql);
+                searchingSale.setLong(1, saleId);
+                ResultSet resultSearch = searchingSale.executeQuery();
 
-            String sql = "SELECT * FROM venda WHERE id = ?";
-            PreparedStatement searchingSale = connection.prepareStatement(sql);
-            searchingSale.setLong(1, saleId);
-            ResultSet resultSearch = searchingSale.executeQuery();
-
-            if (resultSearch.next()) {
-                System.out.println("ID da venda: " + resultSearch.getLong(1));
-                System.out.println("Valor total da venda: " + resultSearch.getInt(2));
-                System.out.println("ID do cliente da venda: " + resultSearch.getLong(3));
-                System.out.println("ID do vendedor da venda: " + resultSearch.getLong(4));
-                System.out.println("Horário do cadastro da venda: " + resultSearch.getInt(5));
-            } else {
-                System.out.println("Não foi possível encontrar nenhuma venda com este ID.");
+                if (resultSearch.next()) {
+                    System.out.println("ID da venda: " + resultSearch.getLong(1));
+                    System.out.println("Valor total da venda: " + resultSearch.getInt(2));
+                    System.out.println("ID do cliente da venda: " + resultSearch.getLong(3));
+                    System.out.println("ID do vendedor da venda: " + resultSearch.getLong(4));
+                    System.out.println("Horário do cadastro da venda: " + resultSearch.getInt(5));
+                } else {
+                    System.out.println("Não foi possível encontrar nenhuma venda com este ID.");
+                }
+            } catch (SQLException exception) {
+                System.err.println("Erro ao pesquisar a venda: " + exception.getMessage());
+                exception.printStackTrace();
             }
         } catch (SQLException exception) {
-            exception.getMessage();
+            System.err.println("Erro ao se conectar ao banco: " + exception.getMessage());
             exception.printStackTrace();
         }
     }
@@ -99,24 +110,27 @@ public class Search {
 
             System.out.println("Qual a descrição do fornecedor?");
             String descricao = sc.nextLine();
+            try {
+                String sql = "SELECT * FROM fornecedor WHERE UPPER(DESCRICAO) = UPPER(?)";
+                PreparedStatement searchingSupplier = connection.prepareStatement(sql);
+                searchingSupplier.setString(1, descricao);
+                ResultSet resultSearch = searchingSupplier.executeQuery();
 
-            String sql = "SELECT * FROM fornecedor WHERE UPPER(DESCRICAO) = UPPER(?)";
-            PreparedStatement searchingSupplier = connection.prepareStatement(sql);
-            searchingSupplier.setString(1, descricao);
-            ResultSet resultSearch = searchingSupplier.executeQuery();
-
-            if (resultSearch.next()) {
-                System.out.println("ID do fornecedor: " + resultSearch.getLong(1));
-                System.out.println("Descrição do fornecedor: " + resultSearch.getString(2));
-                System.out.println("Contato do fornecedor: " + resultSearch.getString(3));
-                System.out.println("Endereço do fornecedor: " + resultSearch.getString(4));
-                System.out.println("Horário do cadastro do fornecedor: " + resultSearch.getInt(5));
-            } else {
-                System.out.println("Não foi possível encontrar um fornecedor com esta descrição.");
+                if (resultSearch.next()) {
+                    System.out.println("ID do fornecedor: " + resultSearch.getLong(1));
+                    System.out.println("Descrição do fornecedor: " + resultSearch.getString(2));
+                    System.out.println("Contato do fornecedor: " + resultSearch.getString(3));
+                    System.out.println("Endereço do fornecedor: " + resultSearch.getString(4));
+                    System.out.println("Horário do cadastro do fornecedor: " + resultSearch.getInt(5));
+                } else {
+                    System.out.println("Não foi possível encontrar um fornecedor com esta descrição.");
+                }
+            } catch (SQLException exception) {
+                System.err.println("Erro ao pesquisar o fornecedor: " + exception.getMessage());
+                exception.printStackTrace();
             }
-
         } catch (SQLException exception) {
-            exception.getMessage();
+            System.err.println("Erro ao se conectar ao banco: " + exception.getMessage());
             exception.printStackTrace();
         }
     }
@@ -129,25 +143,28 @@ public class Search {
             System.out.println("Qual é o nome do vendedor?");
             String nome = sc.nextLine();
 
-            String sql = "SELECT * FROM vendedor WHERE UPPER(nome) = UPPER(?)";
-            PreparedStatement searchingSeller = connection.prepareStatement(sql);
-            searchingSeller.setString(1, nome);
-            ResultSet resultSearch = searchingSeller.executeQuery();
+            try {
+                String sql = "SELECT * FROM vendedor WHERE UPPER(nome) = UPPER(?)";
+                PreparedStatement searchingSeller = connection.prepareStatement(sql);
+                searchingSeller.setString(1, nome);
+                ResultSet resultSearch = searchingSeller.executeQuery();
 
-            if (resultSearch.next()) {
-                System.out.println("ID do vendedor: " + resultSearch.getLong(1));
-                System.out.println("Nome do vendedor: " + resultSearch.getString(2));
-                System.out.println("Número de vendas do vendedor: " + resultSearch.getInt(3));
-                System.out.println("Total do valor de suas vendas: " + resultSearch.getInt(4));
-                System.out.println("Horário do registro do vendedor: " + resultSearch.getInt(5));
-            } else {
-                System.out.println("Não foi possível encontrar um vendedor com este nome.");
+                if (resultSearch.next()) {
+                    System.out.println("ID do vendedor: " + resultSearch.getLong(1));
+                    System.out.println("Nome do vendedor: " + resultSearch.getString(2));
+                    System.out.println("Número de vendas do vendedor: " + resultSearch.getInt(3));
+                    System.out.println("Total do valor de suas vendas: " + resultSearch.getInt(4));
+                    System.out.println("Horário do registro do vendedor: " + resultSearch.getInt(5));
+                } else {
+                    System.out.println("Não foi possível encontrar um vendedor com este nome.");
+                }
+            } catch (SQLException exception) {
+                System.err.println("Erro ao selecionar o vendedor: " + exception.getMessage());
+                exception.printStackTrace();
             }
-
         } catch (SQLException exception) {
-            exception.getMessage();
+            System.err.println("Erro ao se conectar ao banco: " + exception.getMessage());
             exception.printStackTrace();
         }
-
     }
 }
